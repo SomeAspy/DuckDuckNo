c00ec43afa3d02e9d8635401d3a88ab388f23d7b
Seth Carlson <89495053+DrNoSleep@users.noreply.github.com>
2021-11-11 14:08:17 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+var he = document.getElementById("header");
+var baseurl = "https://curly-limit-5a5e.drns.workers.dev/?url=https://api.duckduckgo.com/?q=";
+var options = "&format=json";
+
+async function go() {
+    var input = document.getElementById("search").value;
+    let response = await fetch(baseurl + input + options);
+    let data = await response.json();
+    let json = data.list;
+    he.innerHTML = "<h3>" + data.Heading + "</h3><img src='https://api.duckduckgo.com" + data.Image + "'></img><br><br>" + data.Abstract + "<br> - " + data.AbstractSource;
+}
