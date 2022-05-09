6b33bcc5fa61bb9a6e01bd1d18c95a6231c64929
Aiden <amb@aspy.dev>
2022-05-08 21:47:05 -0400

    Begin work on the results page remove old results system Add Robots.txt and vercel.json Rename Search to Home

--- src/app.jsx
+++ src/app.jsx
@@ -6,13 +6,15 @@ import { render } from 'solid-js/web';
 import { lazy } from 'solid-js';
 import { Route, Router, Routes } from 'solid-app-router';
 
+const Home = lazy(() => import('./routes/home.jsx'));
 const Search = lazy(() => import('./routes/search.jsx'));
 
 render(
     () => (
         <Router>
             <Routes>
-                <Route path='/' component={Search} />
+                <Route path='/' component={Home} />
+                <Route path='/search' component={Search} />
             </Routes>
         </Router>
     ),
--- src/routes/search.jsx
+++ src/routes/search.jsx
@@ -3,37 +3,29 @@
 // This software is released under the MIT License.
 // https://opensource.org/licenses/MIT
 
-import '../CSS/search.css';
-import logo from '../assets/logo.svg';
+const params = new Proxy(new URLSearchParams(window.location.search), {
+    get: (searchParams, prop) => searchParams.get(prop),
+});
+const query = params.q;
 
-const handleSearch = (event) => {
-    if (event.key === 'Enter') {
-        event.preventDefault();
-        window.location.replace(
-            `https://duckduckgo.com/?q=${
-                document.getElementById('userSearch').value
-            }`,
-        );
-    }
-};
-
-export default function () {
-    return (
-        <>
-            <div className='logo'>
-                <img src={logo} alt='Upside down DuckDuckGo logo' />
-                <h1 className='logoText'>Duck Duck No</h1>
-            </div>
-            <div className='search'>
-                <input
-                    className='search'
-                    type='text'
-                    name='userSearch'
-                    placeholder='Search The Web For Anything'
-                    id='userSearch'
-                    autofocus
-                    onKeyPress={handleSearch}></input>
-            </div>
-        </>
-    );
+if (!query) {
+    window.location.replace('/');
 }
+
+async function fetchResults(searchQuery) {
+    let results;
+
+    await fetch(
+        `https://cors-proxy.aspy.workers.dev?https://api.duckduckgo.com/?q=${searchQuery}&format=json&no_redirect=1`,
+    )
+        // We need a cors proxy because the duckduckgo api doesn't allow cross-origin requests for some inane reason. I don't know why.
+        // I route it through a cloudflare worker. This is a bit of a hack, but it works. The code for the worker wasn't written by me.
+        // Although CORS Proxies are inherently insecure, we aren't using them to route sensitive data.
+        .then((response) => response.json())
+        .then((data) => {
+            results = data.RelatedTopics;
+        });
+    return results;
+}
+
+console.log(await fetchResults(query));
