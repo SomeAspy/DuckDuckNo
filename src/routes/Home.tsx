0f0abe70b30e525adc074564e0e9047680f99502
Aiden <amb@aspy.dev>
2023-09-02 21:59:27 -0400

    Horrid Code

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -2,13 +2,27 @@ import { JSX } from 'solid-js';
 import '../CSS/Home.css'
 
 import Logo from '../assets/logo-optimized.svg'
+import handleSearch from '../TS/search'
 
 export default function Home(): JSX.Element{
+
     return (
         <>
             <div class='logo-element'>
-                <Logo />
-                <h1 class='logoText'>DuckDuckNo</h1>
+                <div class='logo-container'>
+                        <Logo />
+                    </div>
+                <h1 class='logo-text'>Duck Duck No</h1>
+            </div>
+            <div class='search-box'>
+                <input
+                    class='search'
+                    type='text'
+                    name='search'
+                    placeholder='Search The Web'
+                    id='search'
+                    autofocus
+                />
             </div>
         </>
     )
