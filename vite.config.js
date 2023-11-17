038c99b371a7c2d1d036f93be5819f6d92e27f0a
Aiden <amb@aspy.dev>
2023-11-16 20:01:26 -0500

    This is not TSX

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 import solidPlugin from "vite-plugin-solid";
 import webfontDownload from "vite-plugin-webfont-dl";
-import viteCompression from 'vite-plugin-compression';
+import viteCompression from "vite-plugin-compression";
 
 export default defineConfig({
   plugins: [
