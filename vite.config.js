a3d376d0c9e184e58373e5fc1b7e46842fba59f0
Aiden <amb@aspy.dev>
2023-11-16 20:00:54 -0500

    Standardization

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,22 @@
 import { defineConfig } from "vite";
 import solidPlugin from "vite-plugin-solid";
-import { webfontDownload } from "vite-plugin-webfont-dl";
+import webfontDownload from "vite-plugin-webfont-dl";
+import viteCompression from 'vite-plugin-compression';
 
 export default defineConfig({
-  plugins: [solidPlugin(), webfontDownload()],
+  plugins: [
+    solidPlugin(),
+    webfontDownload(),
+    viteCompression({
+      algorithm: "brotliCompress",
+      filter: /\.(js|mjs|ts|tsx|json|css|html)$/i,
+    }),],
   server: {
     port: 3000,
   },
   build: {
     target: "esnext",
+    sourcemap: true
   },
   preview: {
     port: 3000,
