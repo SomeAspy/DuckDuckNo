9deea304cd82d939a82ed3afabca03b38f2a7c40
Aiden <amb@aspy.dev>
2023-12-31 08:56:08 -0500

    Package Updates & Fix Lint

--- vite.config.ts
+++ vite.config.ts
@@ -2,18 +2,12 @@ import { defineConfig } from "vite";
 import solidPlugin from "vite-plugin-solid";
 import webfontDownload from "vite-plugin-webfont-dl";
 import solidSvg from "vite-plugin-solid-svg";
-import viteCompression from "vite-plugin-compression";
 
 export default defineConfig({
     plugins: [
         solidPlugin(),
         webfontDownload(),
-        viteCompression({
-            algorithm: "brotliCompress",
-            filter: /\.(js|mjs|ts|tsx|json|css|html)$/i,
-        }),
         solidSvg({
-            defaultAsComponent: true,
             svgo: {
                 enabled: true,
                 svgoConfig: {
